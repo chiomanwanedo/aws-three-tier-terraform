173d2a87d61e7200f61fb489907a90e14f3b3d63
Azure DevOps Pipeline <buildagent@azuredevops.com>
2026-05-29 12:50:49 +0100

    Add security groups and security group rules

---
+++ terraform/security_group_rules.tf
@@ -0,0 +1,114 @@
+resource "aws_security_group_rule" "alb_egress_ecs" {
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.alb_security_group.id
+  source_security_group_id = aws_security_group.ecs_security_group.id
+}
+
+resource "aws_security_group_rule" "alb_ingress_http" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.alb_security_group.id
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "alb_ingress_https" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.alb_security_group.id
+  cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+
+resource "aws_security_group_rule" "ecs_ingress_http" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_security_group.id
+  source_security_group_id = aws_security_group.alb_security_group.id
+}
+
+
+resource "aws_security_group_rule" "ecs_egress_http" {
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_security_group.id
+  source_security_group_id = aws_security_group.nat_security_group.id
+}
+
+resource "aws_security_group_rule" "ecs_egress_https" {
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_security_group.id
+  source_security_group_id = aws_security_group.nat_security_group.id
+}
+
+
+resource "aws_security_group_rule" "ecs_egress_aurora" {
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_security_group.id
+  source_security_group_id = aws_security_group.aurora_security_group.id
+}
+
+resource "aws_security_group_rule" "ecs_egress_redis" {
+  type                     = "egress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.ecs_security_group.id
+  source_security_group_id = aws_security_group.redis_security_group.id
+}
+
+
+resource "aws_security_group_rule" "redis_egress_ecs" {
+  type                     = "egress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.redis_security_group.id
+  source_security_group_id = aws_security_group.ecs_security_group.id
+}
+
+resource "aws_security_group_rule" "redis_ingress_ecs" {
+  type                     = "ingress"
+  from_port                = 6379
+  to_port                  = 6379
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.redis_security_group.id
+  source_security_group_id = aws_security_group.ecs_security_group.id
+}
+
+
+
+resource "aws_security_group_rule" "aurora_ingress_ecs" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.aurora_security_group.id
+  source_security_group_id = aws_security_group.ecs_security_group.id
+}
+
+resource "aws_security_group_rule" "aurora_egress_ecs" {
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.aurora_security_group.id
+  source_security_group_id = aws_security_group.ecs_security_group.id
+}
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -5,24 +5,38 @@ resource "aws_security_group" "nat_security_group" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-}
-  
+  }
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-}
-
+  }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-}
-  tags = {
-    Name = "three-tier-nat-security-group"
-}
+  }
+  tags = { Name = "three-tier-nat-security-group" }
 }
 
+resource "aws_security_group" "alb_security_group" {
+  vpc_id = aws_vpc.three_tier_vpc.id
+  tags   = { Name = "three-tier-alb-security-group" }
+}
 
+resource "aws_security_group" "ecs_security_group" {
+  vpc_id = aws_vpc.three_tier_vpc.id
+  tags   = { Name = "three-tier-ecs-security-group" }
+}
+
+resource "aws_security_group" "aurora_security_group" {
+  vpc_id = aws_vpc.three_tier_vpc.id
+  tags   = { Name = "three-tier-aurora-security-group" }
+}
+
+resource "aws_security_group" "redis_security_group" {
+  vpc_id = aws_vpc.three_tier_vpc.id
+  tags   = { Name = "three-tier-redis-security-group" }
+}

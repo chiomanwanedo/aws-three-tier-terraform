5af30d725eb1591a0d7c1c712958fef109a540cc
Azure DevOps Pipeline <buildagent@azuredevops.com>
2026-06-01 19:50:54 +0100

    updated alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -37,8 +37,24 @@ resource "aws_lb_listener" "alb_listener" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "alb_listener_https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.acm_certificate_arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_target_group.arn
   }
 }
-

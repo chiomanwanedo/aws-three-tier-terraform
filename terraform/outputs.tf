1bfdb01cffcbffedb625f10aed91938943985d34
Azure DevOps Pipeline <buildagent@azuredevops.com>
2026-06-11 17:35:23 +0100

    add complete outputs for all three tiers

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,7 +3,33 @@ output "alb_dns_name" {
 }
 
 
-
 output "alb_target_group_arn" {
   value = aws_lb_target_group.alb_target_group.arn
+}
+
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.ecs_cluster.name
+}
+
+output "ecs_service_name" {
+  value = aws_ecs_service.ecs_service.name
+}
+
+
+output "ecs_task_definition_arn" {
+  value = aws_ecs_task_definition.ecs_task_definition.arn
+}
+
+
+output "aurora_endpoint" {
+  value = aws_rds_cluster.rds_aurora.endpoint
+}
+
+output "redis_endpoint" {
+  value = aws_elasticache_cluster.elasticache_cluster.cache_nodes[0].address
+}
+
+output "ecr" {
+  value = aws_ecr_repository.ecr_repository.repository_url
 }
